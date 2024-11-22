b09cb1ae9d418fd9fc6b7b065b2b137b6e92ce88
Vasilisa <vasilisa311005@gmail.com>
2024-11-22 11:35:20 +0500

    Начальное состояние

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void f1()
+{
+
+}
+
+void f2()
+{
+
+}
+
+void f3()
+{
+
+}
+
+int main()
+{
+    f1();
+    f2();
+    f3();
+}
